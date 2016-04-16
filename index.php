fe62d6c8d409168007b478bbdd8a1a2cae2016c3
luukk <klaverluuk@gmail.com>
2016-04-16 19:17:53 +0200

    initial commit

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 require_once 'api/index.php';
 require 'app/config/config.php';
 require 'app/config/database.php';
-include 'app/views/head.php';
 include 'app/models/getlanguage.php';
+include 'app/models/setemail.php';
+include 'app/views/head.php';
 
 if(isset($_GET['name']) || isset($_GET['email']) || isset($_GET['age'])){
   $name = $_GET['name'];
